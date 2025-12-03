f46547b55345c3d5073eb1fadedf7e3969cfb3d5
GabRodriMCT <gmagalhaes@mct-esco.com>
2025-12-03 17:45:52 -0300

    alarma sin gpio puesto

--- app/flaskProject/alarma.py
+++ app/flaskProject/alarma.py
@@ -9,17 +9,19 @@ logger = setup_logger("alarma", "alarma.log", level=logging.INFO)
 GPIO_ALARMA = 25
 
 # GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-try:
-    GPIO.setup(GPIO_ALARMA, GPIO.OUT, initial=GPIO.LOW)
-    logger.info("GPIO de alarma inicializado correctamente")
-except Exception as e:
-    logger.error(f"Error inicializando GPIO de alarma: {e}")
-    logger.error(traceback.format_exc())
-
-
+# try:
+#     GPIO.setup(GPIO_ALARMA, GPIO.OUT, initial=GPIO.LOW)
+#     logger.info("GPIO de alarma inicializado correctamente")
+# except Exception as e:
+#     logger.error(f"Error inicializando GPIO de alarma: {e}")
+#     logger.error(traceback.format_exc())
+#
+#
 def activar_alarma_led(duracion=5):
+    logger.warning(f"ALARMA ACTIVADA — LED encendido por {duracion} segundos")
+    return
     try:
         logger.warning(f"ALARMA ACTIVADA — LED encendido por {duracion} segundos")
         GPIO.output(GPIO_ALARMA, GPIO.HIGH)
@@ -30,12 +32,12 @@ def activar_alarma_led(duracion=5):
     finally:
         GPIO.output(GPIO_ALARMA, GPIO.LOW)
         logger.info("LED de alarma apagado")
-
-
-def apagar_alarma_led():
-    try:
-        GPIO.output(GPIO_ALARMA, GPIO.LOW)
-        logger.info("LED de alarma apagado manualmente")
-    except Exception as e:
-        logger.error(f"Error apagando alarma LED: {e}")
-        logger.error(traceback.format_exc())
+#
+#
+# def apagar_alarma_led():
+#     try:
+#         GPIO.output(GPIO_ALARMA, GPIO.LOW)
+#         logger.info("LED de alarma apagado manualmente")
+#     except Exception as e:
+#         logger.error(f"Error apagando alarma LED: {e}")
+#         logger.error(traceback.format_exc())
